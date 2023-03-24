b14550e72fd4b42bd72b798f036b5005b2aca417
Ogomo-constantine <ogomoshiro@gmail.com>
2023-03-23 22:27:11 -0700

    stack,queue,lifo,fifo

--- monty_funcs3.c
+++ monty_funcs3.c
@@ -16,7 +16,8 @@ void monty_nop(stack_t **stack, unsigned int line_number)
 }
 
 /**
- * monty_pchar - prints the character in the top value node of a stack_linked list
+ * monty_pchar - prints the character in the top value node
+ * of a stack_linked list
  * @stack: a pointer to the top mode of stack_t linked list
  * @line_number: the current working line no. of monty bytecode file
  */
@@ -32,7 +33,7 @@ void monty_pchar(stack_t **stack, unsigned int line_number)
 		set_op_tok_error(pchar_error(line_number, "value out of range"));
 		return;
 	}
-	printf("%c\n",(*stack)->next->n);
+	printf("%c\n", (*stack)->next->n);
 }
 
 /**
